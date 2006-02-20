584a2d0cb3d5dc29b100c26cc0d6b59a33d244ca
cmsbuild <>
2006-02-20 15:16:10 +0000

    This commit was manufactured by cvs2git to create branch 'DataFormats- TrackReco-CMSSW_0_5_1_branch'.
    Sprout from master 2006-02-20 15:16:08 UTC Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch> 'common extension implementation moved to base class template TrackExtension'
    Delete:
        BuildFile
        TagLog
        doc/html/index.html
        doc/html/overview.html
        interface/HelixParameters.h
        interface/RecHit.h
        interface/RecHitFwd.h
        interface/Track.h
        interface/TrackBase.h
        interface/TrackExtension.h
        interface/TrackExtra.h
        interface/TrackExtraFwd.h
        interface/TrackFwd.h
        src/HelixParameters.cc
        src/RecHit.cc
        src/Track.cc
        src/TrackBase.cc
        src/TrackExtra.cc
        src/classes.h
        test/BuildFile
        test/testHelixParameters.cc
        test/testRunner.cpp
        test/testTrack.cc

--- src/TrackExtra.cc
+++
@@ -1,9 +0,0 @@
-#include "DataFormats/TrackReco/interface/TrackExtra.h"
-using namespace reco;
-
-TrackExtra::TrackExtra( const Point & outerPosition, const Vector & outerMomentum, 
-			bool ok ) :
-  outerPosition_( outerPosition ), outerMomentum_( outerMomentum ), outerOk_( ok ) {
-}
-
-
